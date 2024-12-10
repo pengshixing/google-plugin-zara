22936f0f0e166b26208b6488685360e054d60c78
pengshixing <pengshixing17347856051@outlook.com>
2024-12-10 13:33:56 +0800

    test

--- popup.js
+++ popup.js
@@ -1,1 +1,26 @@
 // popup.js 文件可以包含一些初始化逻辑或事件处理代码
+console.log('popup.js 已加载');
+document.getElementById('getCurrentTime').addEventListener('click', () => {
+  console.log(new Date().valueOf());
+  
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Popup.js loaded!");
+
+  // Handle button click
+  document.getElementById("getCurrentTime").addEventListener("click", () => {
+    console.log("Button clicked!");
+  });
+});
+
+setTimeout(() => {
+  console.log('popup.js 1000');
+}, 1000);
+
+
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === 'complete' || document.readyState === 'interactive') {
+      console.log('popup.js 已加载');
+    }
+});
